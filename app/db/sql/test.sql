a977aa3c788616647b82715bdfc9ec0646908af3
karachunYaraslau <karacuna396@gmail.com>
2025-10-29 11:35:28 +0300

    Add test sql script

---
+++ app/db/sql/test.sql
@@ -0,0 +1,8 @@
+INSERT INTO main.categories (user_id, name, type, created_at)
+VALUES (1, 'Магазин', 'expense', '2025-10-28 15:09:32.38117+03')
+
+SELECT * FROM main.categories;
+
+SELECT * FROM main.users
+
+SELECT * FROM main.transactions
