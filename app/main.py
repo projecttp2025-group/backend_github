42c348d1940b08ac774d8b58df95ae7c02740ada
IlyaGridyushko <gridilya2@gmail.com>
2025-12-21 15:50:15 +0300

    localhost added

--- app/main.py
+++ app/main.py
@@ -20,6 +20,14 @@ app = FastAPI(
     openapi_url="/openapi.json",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1")
 
 
